0b938d3395ff38f9600b39c609c21afa58c770e4
pokutuna <popopopopokutuna@gmail.com>
2018-08-22 17:29:42 +0900

    save html each frames

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     chrome.pageAction.show(tabId);
 });
 
-// { tab.id : { url, dump, dumpTimer, reloadTimer } }
+// { tab.id : { url, mhtml, mhtmlTimer, frames, framesTimer, reloadTimer, reloadCount } }
 let bombs = {};
 
 chrome.pageAction.onClicked.addListener((tab) => {
@@ -18,24 +18,46 @@ function start(tab) {
     console.log(`${tab.id}: The bomb has been planted.`);
     setIcon(tab.id, true);
 
-    let obj = { url: tab.url };
+    let obj = {
+        url: tab.url,
+        reloadCount: 0
+    };
 
     let updateDump = () => {
-        chrome.pageCapture.saveAsMHTML({ tabId: tab.id }, (blob) => obj.dump = blob);
+        chrome.pageCapture.saveAsMHTML({ tabId: tab.id }, (blob) => obj.mhtml = blob);
         setIcon(tab.id, true);
     };
 
-    obj.dumpTimer = setInterval(updateDump, 1000);
+    let updateFrames = () => {
+        chrome.tabs.executeScript(tab.id, {
+            allFrames: true,
+            code: 'JSON.stringify({ url: location.href, html: document.documentElement.innerHTML })'
+        }, function(frames) {
+            if (chrome.runtime.lastError) {
+                console.error(`${tab.id}: ${chrome.runtime.lastError}`);
+            } else {
+                obj.frames = frames;
+            }
+            obj.framesTimer = setTimeout(updateFrames, 1000);
+        });
+    };
+
+    obj.mhtmlTimer = setInterval(updateDump, 1000);
+
+    obj.framesTimer = setTimeout(updateFrames, 1000);
 
     obj.reloadTimer = setInterval(() => {
-        clearInterval(obj.dumpTimer);
+        clearInterval(obj.mhtmlTimer);
+        clearTimeout(obj.framesTimer);
 
         chrome.tabs.reload(tab.id, null, () => {
-            obj.dumpTimer = setInterval(updateDump, 1000);
+            obj.reloadCount += 1;
+            obj.mhtmlTimer = setInterval(updateDump, 1000);
+            obj.framesTimer = setTimeout(updateFrames, 1000);
         });
         setIcon(tab.id, true);
         console.log(`${tab.id} reloaded.`);
-    }, 1000 * 60 * 4); // 4 min
+    }, 1000 * 60 * 1); // 1 min
 
     bombs[tab.id] = obj;
 }
@@ -46,7 +68,14 @@ function check(tabId, changeInfo) {
 
     if (changeInfo.url && changeInfo.url !== obj.url) {
         console.log(`${tabId} captured at ${(new Date()).toISOString()}`);
-        download(`${tabId}-${Date.now()}.mhtml`, obj.dump);
+        download(
+            `${tabId}-${Date.now()}.mhtml`,
+            obj.mhtml
+        );
+        download(
+            `${tabId}-${Date.now()}.json`,
+            new Blob([ JSON.stringify(obj.frames.map((str) => JSON.parse(str))) ], { type: 'application/json' })
+        );
     }
 }
 
@@ -55,7 +84,7 @@ function stop(tabId) {
     if (!obj) return;
 
     setIcon(tabId, false);
-    clearInterval(obj.dumpTimer);
+    clearInterval(obj.mhtmlTimer);
     clearInterval(obj.reloadTimer);
 
     delete bombs[tabId];
